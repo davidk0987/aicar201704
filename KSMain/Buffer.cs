65777b82ce62b50b1fa56e3aa7d7c0a1a5e34269
davidk0987 <48925303+davidk0987@users.noreply.github.com>
2019-03-26 04:28:34 +0800

    Add files via upload

---
+++ KSMain/Buffer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSMain
+{
+    public class Buffer
+    {
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr CreatePtr();
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static void DeletePtr(IntPtr _point);
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static int ConvSignal(IntPtr _point, byte[] data, int len, byte[] data_0101, ref int len_0101, byte[] data_0806, ref int len_0806);
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static void ClearBuffer(IntPtr _point);
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static int GetBufferLen(IntPtr _point);
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static byte GetAt(IntPtr _point, int index);
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static bool Append(IntPtr _point, byte[] data, int len);
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static int Delete(IntPtr _point, int nSize);
+
+        [DllImport("Buffer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static int Read(IntPtr _point, byte[] data, int nSize);
+
+        private IntPtr _iPtrPoint = IntPtr.Zero;
+        public Buffer()
+        {
+            _iPtrPoint = CreatePtr();
+        }
+
+        ~Buffer()
+        {
+            if(_iPtrPoint!=IntPtr.Zero)
+            {
+                DeletePtr(_iPtrPoint);
+                _iPtrPoint = IntPtr.Zero;
+            }
+        }
+
+        public int ConvSignal_Int(byte[] data, int len, byte[] data_0101, ref int len_0101, byte[] data_0806, ref int len_0806)
+        {
+            if (_iPtrPoint != IntPtr.Zero)
+            {
+                return ConvSignal(_iPtrPoint, data, len,  data_0101, ref len_0101,  data_0806, ref len_0806);
+            }
+
+            return -100;
+        }
+
+        public bool Buff_Append(byte[] data)
+        {
+            if (_iPtrPoint != IntPtr.Zero)
+            {
+                return Append(_iPtrPoint, data, data.Length);
+            }
+            return false;
+        }
+
+        public int Buff_GetBufferLen()
+        {
+            if (_iPtrPoint != IntPtr.Zero)
+            {
+                return GetBufferLen(_iPtrPoint);
+            }
+            return -1;
+        }
+
+        public byte Buff_GetAt(int index)
+        {
+            if (_iPtrPoint != IntPtr.Zero)
+            {
+                return GetAt(_iPtrPoint, index);
+            }
+            return 0;
+        }
+
+        public int Buff_Delete(int nSize)
+        {
+            if (_iPtrPoint != IntPtr.Zero)
+            {
+                return Delete(_iPtrPoint, nSize);
+            }
+            return 0;
+        }
+
+        public int Buff_Read(byte[] data, int nSize)
+        {
+            if (_iPtrPoint != IntPtr.Zero)
+            {
+                return Read(_iPtrPoint, data, nSize);
+            }
+            return 0;
+        }
+    }
+}
---
+++ KSMain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffer _buffer = new Buffer();
+
+            byte[] data = System.Text.ASCIIEncoding.Default.GetBytes("$KSXT,20190106065841.80,113.49058882,23.44375600,22.5517,0.00,0.00,80.92,0.036,,3,0,0,17,-0.986,1470.338,-14.213,0.036,0.006,0.072,00000000000000000000000000000,00000000000000000000000000000,11111000011101111100000000001,0,0,0,LOCK/OFF,0.000,Z02B11836100001932D,KSXT_V1.6_201901011651,20190210,C002,113.49059847,23.43047988,36.59481000,FD979E6B71437CBF73C5FE0C69DDA94E,2.463,0.904,0.014,0.024,1,0.000,0.000,0.000,R01B11900600000042A,20210105,0001*36");
+            byte[] data_0101 = new byte[1024];
+            byte[] data_0806 = new byte[1024];
+            int len_0101 = 0, len_0806 = 0;
+            _buffer.ConvSignal_Int(data, data.Length, data_0101,ref len_0101, data_0806,ref len_0806);
+
+
+            string str = string.Format("0101:{0}   0806:{1}", len_0101, len_0806);
+            Console.WriteLine(str);
+        }
+    }
+}
